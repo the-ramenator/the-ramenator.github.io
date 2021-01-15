4a0c8cd0f85cd05fbc2a7045f0fcb0fff8241642
neildaniel00 <75753005+neildaniel00@users.noreply.github.com>
2021-01-15 11:45:09 -0500

    Create main.js

---
+++ platformer/main.js
@@ -0,0 +1,58 @@
+const gameCanvas = document.getElementById("game-canvas");
+const gameContext = gameCanvas.getContext("2d");
+gameCanvas.width = window.innerWidth;
+gameCanvas.height = window.innerHeight;
+
+var previousTimeStamp = 0;
+var maxDeltaTime = 20;
+
+var player;
+var camera;
+
+function drawGameObject(gameObject) {
+    gameContext.fillStyle = gameObject.color;
+    let positionOnScreen = copyVector(gameObject.position);
+    subVectors(positionOnScreen, camera.position);
+    positionOnScreen.x += gameCanvas.width / 2;
+    positionOnScreen.y += gameCanvas.height / 2;
+    gameContext.fillRect(positionOnScreen.x, positionOnScreen.y,
+        gameObject.scale.x, gameObject.scale.y);
+}
+
+function drawGameScreen() {
+    gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    gameObjects.forEach(drawGameObject);
+}
+
+window.addEventListener('load', () => {
+    player = Player(Vector2D(140,200));
+    camera = Camera();
+    Block(Vector2D(10, 400), Vector2D(500, 60));
+    Block(Vector2D(410, 260), Vector2D(500, 60));
+    Block(Vector2D(700, 400), Vector2D(500, 60));
+    Block(Vector2D(1400, 400), Vector2D(500, 60));
+    Block(Vector2D(1900, 250), Vector2D(500, 60));
+    Block(Vector2D(1900, 2050), Vector2D(500, 60));
+    Enemy(Vector2D(1400,-400));
+    Enemy(Vector2D(460,-100));
+    Enemy(Vector2D(1900,-250));
+    Enemy(Vector2D(1900,-350));
+    Enemy(Vector2D(1900,-450));
+    window.requestAnimationFrame(frame);
+});
+
+function frame(timeStamp) {
+    if (!previousTimeStamp) {
+        previousTimeStamp = timeStamp;
+    }
+    let deltaTime = timeStamp - previousTimeStamp;
+    if (deltaTime > maxDeltaTime) {
+        deltaTime = maxDeltaTime;
+    }
+    updateGameObjects(deltaTime);
+    gameObjects = gameObjects.filter(gameObject => {
+        return !gameObject.destroy;
+    });
+    drawGameScreen();
+    window.requestAnimationFrame(frame);
+}
